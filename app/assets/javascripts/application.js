28e1d56215718615e3764fc9e343103ee4e6c1ce
Do Huy Truong <david@Dos-MacBook-Pro.local>
2019-05-07 13:30:46 +0700

    ypok-52_ajax_for_end_date

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require jquery
-//= require toastr
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min 
+//= require jquery-ui/datepicker
+//= require toastr
 //= require_tree .
